32cf39ee626f1250802081350e934bce780592d0
BSoD_ultimate <1111>
2018-05-14 00:39:12 +0800

    1.add register URL attribute in server panel. 2.fix a server modifying issue.

--- TOPLauncher/toplauncher_zh.ts
+++ TOPLauncher/toplauncher_zh.ts
@@ -72,42 +72,47 @@
         <translation>服务器地址:</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="344"/>
+        <location filename="DlgSettings.ui" line="308"/>
+        <source>Registration URL:</source>
+        <translation>用户注册URL:</translation>
+    </message>
+    <message>
+        <location filename="DlgSettings.ui" line="357"/>
         <source>Save changes</source>
         <translation>保存更改</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="351"/>
+        <location filename="DlgSettings.ui" line="364"/>
         <source>Cancel</source>
         <translation>取消</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="358"/>
+        <location filename="DlgSettings.ui" line="371"/>
         <source>Remove</source>
         <translation>删除</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="399"/>
+        <location filename="DlgSettings.ui" line="412"/>
         <source>Game Control Settings</source>
         <translation>游戏控制设置</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="723"/>
+        <location filename="DlgSettings.ui" line="736"/>
         <source>Line clear delay (the less value, the quicker next piece appears after lines were cleared)</source>
-        <translation>消行延迟（数值越小，消行与下一个方块出现之间的时间间隔越短）</translation>
+        <translation>消行延迟（数值越小，消行事件与下一个方块出现之间的时间间隔越短）</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="759"/>
+        <location filename="DlgSettings.ui" line="772"/>
         <source>Long</source>
         <translation>长</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="803"/>
+        <location filename="DlgSettings.ui" line="816"/>
         <source>Short</source>
         <translation>短</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="836"/>
+        <location filename="DlgSettings.ui" line="849"/>
         <source>Apply New Control Settings</source>
         <translation>应用新控制设置</translation>
     </message>
@@ -116,39 +121,39 @@
         <translation type="vanished">按键灵敏度设置</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="408"/>
+        <location filename="DlgSettings.ui" line="421"/>
         <source>Move left/right sensitivity (the less value, the more sensitive a piece moves)</source>
         <translation>左右移动灵敏度（数值越小，移动灵敏度越高）</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="444"/>
+        <location filename="DlgSettings.ui" line="457"/>
         <source>Obtuse</source>
         <translation>迟钝</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="485"/>
+        <location filename="DlgSettings.ui" line="498"/>
         <source>Sensitive</source>
         <translation>灵敏</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="513"/>
+        <location filename="DlgSettings.ui" line="526"/>
         <source>Move left/right speed (the less value, the faster when a piece moves)</source>
         <translation>左右移动速度（数值越小，左右持续移动的速度越快）</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="549"/>
-        <location filename="DlgSettings.ui" line="654"/>
+        <location filename="DlgSettings.ui" line="562"/>
+        <location filename="DlgSettings.ui" line="667"/>
         <source>Slow</source>
         <translation>慢</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="590"/>
-        <location filename="DlgSettings.ui" line="695"/>
+        <location filename="DlgSettings.ui" line="603"/>
+        <location filename="DlgSettings.ui" line="708"/>
         <source>Fast</source>
         <translation>快</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="618"/>
+        <location filename="DlgSettings.ui" line="631"/>
         <source>Soft-drop speed (the less value, the quicker when a piece moves down by player)</source>
         <translation>软降速度（数值越小，手动软降的速度越快）</translation>
     </message>
@@ -157,7 +162,7 @@
         <translation type="vanished">应用灵敏度设置</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="849"/>
+        <location filename="DlgSettings.ui" line="862"/>
         <source>Reset</source>
         <translation>重置</translation>
     </message>
@@ -178,68 +183,68 @@
         <translation>新建服务器...</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="199"/>
-        <location filename="DlgSettings.cpp" line="296"/>
-        <location filename="DlgSettings.cpp" line="304"/>
-        <location filename="DlgSettings.cpp" line="394"/>
+        <location filename="DlgSettings.cpp" line="196"/>
+        <location filename="DlgSettings.cpp" line="298"/>
+        <location filename="DlgSettings.cpp" line="306"/>
+        <location filename="DlgSettings.cpp" line="400"/>
         <source>Error</source>
         <translation>错误</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="200"/>
+        <location filename="DlgSettings.cpp" line="197"/>
         <source>Current server profile is reserved by application, which cannot be modified, removed or conflict with another server profile.</source>
         <translation>当前服务器设置是启动器内置的，该设置不能被更改、删除，或与其它服务器名冲突。</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="246"/>
+        <location filename="DlgSettings.cpp" line="243"/>
         <source>Find where game executable locates...</source>
         <translation>寻找游戏程序所在位置...</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="248"/>
+        <location filename="DlgSettings.cpp" line="245"/>
         <source>Game executable (tetris.exe)</source>
         <translation>TOP客户端程序 (tetris.exe)</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="296"/>
+        <location filename="DlgSettings.cpp" line="298"/>
         <source>The field &quot;Server name&quot; should not empty.</source>
         <translation>服务器名不能为空。</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="304"/>
+        <location filename="DlgSettings.cpp" line="306"/>
         <source>Server name must be unique.</source>
         <translation>服务器名不能与其它的服务器设置冲突。</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="358"/>
+        <location filename="DlgSettings.cpp" line="364"/>
         <source>Would you like to remove the server profile &quot;{}&quot; ? This will remove all saved users who login to this server.</source>
         <translation>要移除服务器设置&quot;{}&quot;吗？所有登录到该服务器的用户也将会被删除。</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="381"/>
+        <location filename="DlgSettings.cpp" line="387"/>
         <source>Would you like to apply the new game control settings? </source>
         <translation>要应用新的游戏控制设置吗？ </translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="390"/>
+        <location filename="DlgSettings.cpp" line="396"/>
         <source>Successfully applied new game control settings. </source>
         <translation>成功应用了新的游戏控制设置。 </translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="394"/>
+        <location filename="DlgSettings.cpp" line="400"/>
         <source>Unable to apply new game control settings. </source>
         <translation>无法应用新的游戏控制设置。 </translation>
     </message>
     <message>
         <location filename="DlgLanguage.cpp" line="39"/>
-        <location filename="DlgSettings.cpp" line="228"/>
+        <location filename="DlgSettings.cpp" line="225"/>
         <source>Would you like to use your system language &quot;{}&quot; as the display language?</source>
         <translation>你要把系统语言&quot;{}&quot;设置为启动器显示语言吗？</translation>
     </message>
     <message>
         <location filename="DlgLanguage.cpp" line="44"/>
-        <location filename="DlgSettings.cpp" line="233"/>
-        <location filename="DlgSettings.cpp" line="362"/>
+        <location filename="DlgSettings.cpp" line="230"/>
+        <location filename="DlgSettings.cpp" line="368"/>
         <source>Question</source>
         <translation>问题</translation>
     </message>
@@ -248,7 +253,7 @@
         <translation type="vanished">要应用新的按键灵敏度设置吗？ </translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="390"/>
+        <location filename="DlgSettings.cpp" line="396"/>
         <source>Info</source>
         <translation>提示</translation>
     </message>
@@ -267,7 +272,7 @@
         <translation>要移除用户 &quot;{}&quot; 吗？该用户在服务器 &quot;{}&quot; 上登录。 </translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="381"/>
+        <location filename="DlgSettings.cpp" line="387"/>
         <location filename="LoginUserItem.cpp" line="37"/>
         <source>Caution</source>
         <translation>提示</translation>
@@ -322,57 +327,57 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="171"/>
+        <location filename="TOPLauncher.ui" line="175"/>
         <source>Login</source>
         <translation>登录</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="207"/>
+        <location filename="TOPLauncher.ui" line="211"/>
         <source>Username:</source>
         <translation>用户名:</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="220"/>
+        <location filename="TOPLauncher.ui" line="224"/>
         <source>Password:</source>
         <translation>密码:</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="282"/>
+        <location filename="TOPLauncher.ui" line="286"/>
         <source>Remember Password</source>
         <translation>记住密码</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="308"/>
+        <location filename="TOPLauncher.ui" line="312"/>
         <source>Start</source>
         <translation>启动</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="320"/>
+        <location filename="TOPLauncher.ui" line="324"/>
         <source>Language...</source>
         <translation>选择语言...</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="330"/>
+        <location filename="TOPLauncher.ui" line="334"/>
         <source>Register</source>
         <translation>注册账户</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="340"/>
+        <location filename="TOPLauncher.ui" line="344"/>
         <source>Forum</source>
         <translation>论坛</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="407"/>
+        <location filename="TOPLauncher.ui" line="411"/>
         <source>Settings...</source>
         <translation>设置...</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="414"/>
+        <location filename="TOPLauncher.ui" line="418"/>
         <source>Select a server:</source>
         <translation>选择服务器:</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="444"/>
+        <location filename="TOPLauncher.ui" line="448"/>
         <source>Quit</source>
         <translation>退出</translation>
     </message>
--- TOPLauncher/toplauncher_zh_tw.ts
+++ TOPLauncher/toplauncher_zh_tw.ts
@@ -72,42 +72,47 @@
         <translation>伺服器地址:</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="344"/>
+        <location filename="DlgSettings.ui" line="308"/>
+        <source>Registration URL:</source>
+        <translation>賬戶注冊URL:</translation>
+    </message>
+    <message>
+        <location filename="DlgSettings.ui" line="357"/>
         <source>Save changes</source>
         <translation>保存更改</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="351"/>
+        <location filename="DlgSettings.ui" line="364"/>
         <source>Cancel</source>
         <translation>取消</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="358"/>
+        <location filename="DlgSettings.ui" line="371"/>
         <source>Remove</source>
         <translation>刪除</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="399"/>
+        <location filename="DlgSettings.ui" line="412"/>
         <source>Game Control Settings</source>
         <translation>游戲控制設置</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="723"/>
+        <location filename="DlgSettings.ui" line="736"/>
         <source>Line clear delay (the less value, the quicker next piece appears after lines were cleared)</source>
-        <translation>消行延遲（數值越小，消行與下一個方塊出現之間的時間間隔越短）</translation>
+        <translation>消行延遲（數值越小，消行事件與下一個方塊出現之間的時間間隔越短）</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="759"/>
+        <location filename="DlgSettings.ui" line="772"/>
         <source>Long</source>
         <translation>長</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="803"/>
+        <location filename="DlgSettings.ui" line="816"/>
         <source>Short</source>
         <translation>短</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="836"/>
+        <location filename="DlgSettings.ui" line="849"/>
         <source>Apply New Control Settings</source>
         <translation>應用新控制設置</translation>
     </message>
@@ -116,39 +121,39 @@
         <translation type="vanished">按鍵靈敏度設置</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="408"/>
+        <location filename="DlgSettings.ui" line="421"/>
         <source>Move left/right sensitivity (the less value, the more sensitive a piece moves)</source>
         <translation>左右移動敏感度（數值越小，移動敏感度越高）</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="444"/>
+        <location filename="DlgSettings.ui" line="457"/>
         <source>Obtuse</source>
         <translation>遲鈍</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="485"/>
+        <location filename="DlgSettings.ui" line="498"/>
         <source>Sensitive</source>
         <translation>敏感</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="513"/>
+        <location filename="DlgSettings.ui" line="526"/>
         <source>Move left/right speed (the less value, the faster when a piece moves)</source>
         <translation>左右移動速度（數值越小，左右持續移動的速度越快）</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="549"/>
-        <location filename="DlgSettings.ui" line="654"/>
+        <location filename="DlgSettings.ui" line="562"/>
+        <location filename="DlgSettings.ui" line="667"/>
         <source>Slow</source>
         <translation>慢</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="590"/>
-        <location filename="DlgSettings.ui" line="695"/>
+        <location filename="DlgSettings.ui" line="603"/>
+        <location filename="DlgSettings.ui" line="708"/>
         <source>Fast</source>
         <translation>快</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="618"/>
+        <location filename="DlgSettings.ui" line="631"/>
         <source>Soft-drop speed (the less value, the quicker when a piece moves down by player)</source>
         <translation>軟降速度（數值越小，手動軟降的速度越快）</translation>
     </message>
@@ -157,7 +162,7 @@
         <translation type="vanished">應用靈敏度設置</translation>
     </message>
     <message>
-        <location filename="DlgSettings.ui" line="849"/>
+        <location filename="DlgSettings.ui" line="862"/>
         <source>Reset</source>
         <translation>重置</translation>
     </message>
@@ -178,68 +183,68 @@
         <translation>新建伺服器...</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="199"/>
-        <location filename="DlgSettings.cpp" line="296"/>
-        <location filename="DlgSettings.cpp" line="304"/>
-        <location filename="DlgSettings.cpp" line="394"/>
+        <location filename="DlgSettings.cpp" line="196"/>
+        <location filename="DlgSettings.cpp" line="298"/>
+        <location filename="DlgSettings.cpp" line="306"/>
+        <location filename="DlgSettings.cpp" line="400"/>
         <source>Error</source>
         <translation>錯誤</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="200"/>
+        <location filename="DlgSettings.cpp" line="197"/>
         <source>Current server profile is reserved by application, which cannot be modified, removed or conflict with another server profile.</source>
         <translation>當前伺服器設置是啟動器內置的，該設置不能被更改、刪除，或與其它伺服器名衝突。</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="246"/>
+        <location filename="DlgSettings.cpp" line="243"/>
         <source>Find where game executable locates...</source>
         <translation>尋找游戲程序所在位置...</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="248"/>
+        <location filename="DlgSettings.cpp" line="245"/>
         <source>Game executable (tetris.exe)</source>
         <translation>TOP客戶端程序 (tetris.exe)</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="296"/>
+        <location filename="DlgSettings.cpp" line="298"/>
         <source>The field &quot;Server name&quot; should not empty.</source>
         <translation>伺服器名不能為空。</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="304"/>
+        <location filename="DlgSettings.cpp" line="306"/>
         <source>Server name must be unique.</source>
         <translation>伺服器名不能與其它的伺服器設置衝突。</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="358"/>
+        <location filename="DlgSettings.cpp" line="364"/>
         <source>Would you like to remove the server profile &quot;{}&quot; ? This will remove all saved users who login to this server.</source>
         <translation>要移除伺服器設置&quot;{}&quot;嗎？所有登錄到該伺服器的用戶也將會被刪除。</translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="381"/>
+        <location filename="DlgSettings.cpp" line="387"/>
         <source>Would you like to apply the new game control settings? </source>
         <translation>要應用新的游戲控制設置嗎？ </translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="390"/>
+        <location filename="DlgSettings.cpp" line="396"/>
         <source>Successfully applied new game control settings. </source>
         <translation>成功應用了新的游戲控制設置。 </translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="394"/>
+        <location filename="DlgSettings.cpp" line="400"/>
         <source>Unable to apply new game control settings. </source>
         <translation>無法應用新的游戲控制設置。 </translation>
     </message>
     <message>
         <location filename="DlgLanguage.cpp" line="39"/>
-        <location filename="DlgSettings.cpp" line="228"/>
+        <location filename="DlgSettings.cpp" line="225"/>
         <source>Would you like to use your system language &quot;{}&quot; as the display language?</source>
         <translation>你要把系統語言&quot;{}&quot;設置為啟動器顯示語言嗎？</translation>
     </message>
     <message>
         <location filename="DlgLanguage.cpp" line="44"/>
-        <location filename="DlgSettings.cpp" line="233"/>
-        <location filename="DlgSettings.cpp" line="362"/>
+        <location filename="DlgSettings.cpp" line="230"/>
+        <location filename="DlgSettings.cpp" line="368"/>
         <source>Question</source>
         <translation>問題</translation>
     </message>
@@ -248,7 +253,7 @@
         <translation type="vanished">要應用新的按鍵靈敏度設置嗎？ </translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="390"/>
+        <location filename="DlgSettings.cpp" line="396"/>
         <source>Info</source>
         <translation>提示</translation>
     </message>
@@ -267,7 +272,7 @@
         <translation>要移除用戶 &quot;{}&quot; 嗎？該用戶在伺服器 &quot;{}&quot; 上登錄。 </translation>
     </message>
     <message>
-        <location filename="DlgSettings.cpp" line="381"/>
+        <location filename="DlgSettings.cpp" line="387"/>
         <location filename="LoginUserItem.cpp" line="37"/>
         <source>Caution</source>
         <translation>提示</translation>
@@ -322,57 +327,57 @@
         <translation></translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="171"/>
+        <location filename="TOPLauncher.ui" line="175"/>
         <source>Login</source>
         <translation>登入</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="207"/>
+        <location filename="TOPLauncher.ui" line="211"/>
         <source>Username:</source>
         <translation>用戶名:</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="220"/>
+        <location filename="TOPLauncher.ui" line="224"/>
         <source>Password:</source>
         <translation>密碼:</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="282"/>
+        <location filename="TOPLauncher.ui" line="286"/>
         <source>Remember Password</source>
         <translation>記住密碼</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="308"/>
+        <location filename="TOPLauncher.ui" line="312"/>
         <source>Start</source>
         <translation>啟動</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="320"/>
+        <location filename="TOPLauncher.ui" line="324"/>
         <source>Language...</source>
         <translation>選擇語言...</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="330"/>
+        <location filename="TOPLauncher.ui" line="334"/>
         <source>Register</source>
         <translation>注冊賬戶</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="340"/>
+        <location filename="TOPLauncher.ui" line="344"/>
         <source>Forum</source>
         <translation>論壇</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="407"/>
+        <location filename="TOPLauncher.ui" line="411"/>
         <source>Settings...</source>
         <translation>設置...</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="414"/>
+        <location filename="TOPLauncher.ui" line="418"/>
         <source>Select a server:</source>
         <translation>選擇伺服器:</translation>
     </message>
     <message>
-        <location filename="TOPLauncher.ui" line="444"/>
+        <location filename="TOPLauncher.ui" line="448"/>
         <source>Quit</source>
         <translation>退出</translation>
     </message>
