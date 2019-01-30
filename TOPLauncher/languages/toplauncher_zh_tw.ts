c0e95b2147280f61c8f434a0f5e0d6bc208a4a43
BSoD_ultimate <1111>
2019-01-30 11:04:03 +0800

    fix compilation issues

--- TOPLauncher/languages/toplauncher_zh_tw.ts
+++ TOPLauncher/languages/toplauncher_zh_tw.ts
@@ -6,28 +6,24 @@
     <message>
         <location filename="../DlgLanguage.ui" line="47"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgLanguage.h" line="106"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgLanguage.h" line="106"/>
         <source>Switch Language...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgLanguage.ui" line="84"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgLanguage.h" line="107"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgLanguage.h" line="107"/>
         <source>Use System language</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgLanguage.ui" line="104"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgLanguage.h" line="108"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgLanguage.h" line="108"/>
         <source>OK</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgLanguage.ui" line="111"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgLanguage.h" line="109"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgLanguage.h" line="109"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
@@ -37,126 +33,108 @@
     <message>
         <location filename="../DlgSettings.ui" line="41"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="619"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="619"/>
         <source>Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="115"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="620"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="620"/>
         <source>General</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="135"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="621"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="621"/>
         <source>Language:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="145"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="622"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="622"/>
         <source>Game Executable:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="180"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="623"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="623"/>
         <source>Use System language</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="194"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="624"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="624"/>
         <source>Browse...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="220"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="625"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="625"/>
         <source>Server Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="288"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="626"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="626"/>
         <source>Server name:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="298"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="627"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="627"/>
         <source>Server host address:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="308"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="628"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="628"/>
         <source>Registration URL:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="357"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="629"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="629"/>
         <source>Save changes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="364"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="630"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="630"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="371"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="631"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="631"/>
         <source>Remove</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="412"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="632"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="632"/>
         <source>Game Control Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="421"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="633"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="633"/>
         <source>Move left/right sensitivity (the less value, the more sensitive a piece moves)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="457"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="634"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="634"/>
         <source>Obtuse</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="498"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="635"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="635"/>
         <source>Sensitive</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="526"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="636"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="636"/>
         <source>Move left/right speed (the less value, the faster when a piece moves)</source>
         <translation type="unfinished"></translation>
     </message>
@@ -165,8 +143,6 @@
         <location filename="../DlgSettings.ui" line="667"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="637"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="640"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="637"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="640"/>
         <source>Slow</source>
         <translation type="unfinished"></translation>
     </message>
@@ -175,50 +151,42 @@
         <location filename="../DlgSettings.ui" line="708"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="638"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="641"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="638"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="641"/>
         <source>Fast</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="631"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="639"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="639"/>
         <source>Soft-drop speed (the less value, the quicker when a piece moves down by player)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="736"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="642"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="642"/>
         <source>Line clear delay (the less value, the quicker next piece appears after lines were cleared)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="772"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="643"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="643"/>
         <source>Long</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="816"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="644"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="644"/>
         <source>Short</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="849"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="645"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="645"/>
         <source>Apply New Control Settings</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../DlgSettings.ui" line="862"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_DlgSettings.h" line="646"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_DlgSettings.h" line="646"/>
         <source>Reset</source>
         <translation type="unfinished"></translation>
     </message>
@@ -228,7 +196,6 @@
     <message>
         <location filename="../LoginUserItem.ui" line="20"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_LoginUserItem.h" line="88"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_LoginUserItem.h" line="88"/>
         <source>Form</source>
         <translation type="unfinished"></translation>
     </message>
@@ -371,84 +338,72 @@
     <message>
         <location filename="../TOPLauncher.ui" line="41"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_TOPLauncher.h" line="359"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_TOPLauncher.h" line="359"/>
         <source>TOPLauncher</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TOPLauncher.ui" line="175"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_TOPLauncher.h" line="360"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_TOPLauncher.h" line="360"/>
         <source>Login</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TOPLauncher.ui" line="211"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_TOPLauncher.h" line="361"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_TOPLauncher.h" line="361"/>
         <source>Username:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TOPLauncher.ui" line="224"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_TOPLauncher.h" line="362"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_TOPLauncher.h" line="362"/>
         <source>Password:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TOPLauncher.ui" line="286"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_TOPLauncher.h" line="363"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_TOPLauncher.h" line="363"/>
         <source>Remember Password</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TOPLauncher.ui" line="312"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_TOPLauncher.h" line="364"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_TOPLauncher.h" line="364"/>
         <source>Start</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TOPLauncher.ui" line="324"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_TOPLauncher.h" line="365"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_TOPLauncher.h" line="365"/>
         <source>Language...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TOPLauncher.ui" line="334"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_TOPLauncher.h" line="366"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_TOPLauncher.h" line="366"/>
         <source>Register</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TOPLauncher.ui" line="344"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_TOPLauncher.h" line="367"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_TOPLauncher.h" line="367"/>
         <source>Forum</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TOPLauncher.ui" line="411"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_TOPLauncher.h" line="368"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_TOPLauncher.h" line="368"/>
         <source>Settings...</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TOPLauncher.ui" line="418"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_TOPLauncher.h" line="369"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_TOPLauncher.h" line="369"/>
         <source>Select a server:</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
         <location filename="../TOPLauncher.ui" line="448"/>
         <location filename="../TOPLauncher_autogen/include_Debug/ui_TOPLauncher.h" line="370"/>
-        <location filename="../TOPLauncher_autogen/include_RelWithDebInfo/ui_TOPLauncher.h" line="370"/>
         <source>Quit</source>
         <translation type="unfinished"></translation>
     </message>
