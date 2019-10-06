c6fdede3aac56e440ec96f1b9d68de0dd52a4c38
BSoD_ultimate <1111>
2019-10-06 23:40:05 +0800

    Use lupdate/lrelease to implement the multi-language functionality again(maintaining translations in one single JSON file by hand is too difficult). Add tinyxml2 submodule to parse translation files generated by lupdate.

---
+++ TOPLauncher/Resources/translations/zh_CN.ts
@@ -0,0 +1,334 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>DlgLanguage</name>
+    <message>
+        <source>Switch Language...</source>
+        <comment>dlglanguage_window_title</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Use System language</source>
+        <comment>dlglanguage_btn_use_system_lang</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>OK</source>
+        <comment>btn_ok</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Cancel</source>
+        <comment>btn_cancel</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoginUserItem</name>
+    <message>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWidget</name>
+    <message>
+        <source>TOPLauncher</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <comment>mainwidget_group_login</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Username:</source>
+        <comment>mainwidget_login_label_username</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Password:</source>
+        <comment>mainwidget_login_label_password</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remember Password</source>
+        <comment>mainwidget_chk_rememberpwd</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Start</source>
+        <comment>mainwidget_login_btn_start</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Register</source>
+        <comment>mainwidget_login_btn_register</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Forum</source>
+        <comment>mainwidget_login_btn_openforum</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Language...</source>
+        <comment>mainwidget_btn_language</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Select a server:</source>
+        <comment>mainwidget_label_choose_server</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <comment>mainwidget_btn_exit</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Would you like to use your system language &quot;%1&quot; as the display language?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Question</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Could not find translations for your system language &quot;%1&quot;.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Would you like to remove the user &quot;%1&quot; who logins to server &quot;%2&quot;? </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Caution</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>A fatal error has occurred during the startup: 
+%1 
+The program will now exit.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fatal Error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Username &amp; password input box should not empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid server profile.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Could not find where the game executable locates. Startup failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Current server profile does not contain a registration URL.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>New server profile...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Find where the game executable locates...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Game executable (tetris.exe)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>The field &quot;Server name&quot; should not empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Server name must be unique.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Would you like to remove the server profile &quot;%1&quot; ? This will remove all saved users who login to this server.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Would you like to apply the new game control settings? </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Info</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Successfully applied new game control settings. </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Unable to apply new game control settings. </source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SettingsWidget</name>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>General</source>
+        <comment>settingwidget_group_general</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Language:</source>
+        <comment>settingwidget_general_label_language</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Use System language</source>
+        <comment>settingwidget_general_btn_usesystemlang</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Game Executable:</source>
+        <comment>settingwidget_general_label_executable</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Browse...</source>
+        <comment>settingwidget_general_btn_browse</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Server Settings</source>
+        <comment>settingwidget_group_serversettings</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Server name:</source>
+        <comment>settingwidget_serversettings_label_servername</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Server host address:</source>
+        <comment>settingwidget_serversettings_label_serveraddr</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Registration URL:</source>
+        <comment>settingwidget_serversettings_label_regurl</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Save changes</source>
+        <comment>settingwidget_serversettings_btn_save</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Discard changes</source>
+        <comment>settingwidget_serversettings_btn_discard</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <comment>settingwidget_serversettings_btn_remove</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Game Control Settings</source>
+        <comment>settingwidget_group_gamecontrol</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Move left/right sensitivity (the less value, the more sensitive a piece moves)</source>
+        <comment>settingwidget_gamecontrol_move_sensitivity_desc</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Obtuse</source>
+        <comment>settingwidget_gamecontrol_move_sensitivity_high_value</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Sensitive</source>
+        <comment>settingwidget_gamecontrol_move_sensitivity_low_value</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Move left/right speed (the less value, the faster when a piece moves)</source>
+        <comment>settingwidget_gamecontrol_move_speed_desc</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Slow</source>
+        <comment>settingwidget_gamecontrol_move_speed_high_value</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fast</source>
+        <comment>settingwidget_gamecontrol_move_speed_low_value</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Soft-drop speed (the less value, the quicker when a piece moves downwards by player)</source>
+        <comment>settingwidget_gamecontrol_soft_drop_speed_desc</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Slow</source>
+        <comment>settingwidget_gamecontrol_drop_speed_high_value</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Fast</source>
+        <comment>settingwidget_gamecontrol_drop_speed_low_value</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Line clear delay (the shorter the line-clear animation lasts)</source>
+        <comment>settingwidget_gamecontrol_line_clear_delay_desc</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Long</source>
+        <comment>settingwidget_gamecontrol_line_clear_high_value</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Short</source>
+        <comment>settingwidget_gamecontrol_line_clear_low_value</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Apply New Control Settings</source>
+        <comment>settingwidget_gamecontrol_btn_apply</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Reset</source>
+        <comment>settingwidget_gamecontrol_btn_reset</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TOPLauncherClass</name>
+    <message>
+        <source>TOP(Tetris Online Poland) Launcher</source>
+        <comment>main_window_title</comment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
